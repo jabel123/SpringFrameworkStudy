4aaaaea5900f4768219413d373f572d6cc95d615
jabel <jabel123@naver.com>
2019-12-04 17:48:27 +0900

    Make Controller Test
    i made controller test util referenced by toby's springframework

--- src/test/java/com/copocalypse/www/web/HomeControllerTest.java
+++ src/test/java/com/copocalypse/www/web/HomeControllerTest.java
@@ -15,22 +15,23 @@ import org.springframework.mock.web.MockServletConfig;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.copocalypse.www.controller.HomeController;
 import com.copocalypse.www.vo.User;
+import com.copocalypse.www.web.testutil.AbstractDispatcherServletTest;
 import com.copocalypse.www.web.testutil.ConfigurableDispatcherServlet;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration(locations = { "classpath:config/*-context.xml",
-		"classpath:web-context.xml" })
-public class HomeControllerTest {
+@ContextConfiguration(locations = { "classpath:config/*-context.xml", "classpath:web-context.xml" })
+public class HomeControllerTest extends AbstractDispatcherServletTest {
 
 	@Test
 	public void test() throws ServletException, IOException {
 		ConfigurableDispatcherServlet servlet = new ConfigurableDispatcherServlet();
-		servlet.setLocations("classpath:config/*-context.xml",
-				"classpath:web-context.xml");
+		servlet.setLocations("classpath:config/*-context.xml", "classpath:web-context.xml");
 		servlet.init(new MockServletConfig("appServlet"));
 
 		MockHttpServletRequest req = new MockHttpServletRequest("GET", "/sample");
@@ -42,4 +43,14 @@ public class HomeControllerTest {
 		User user = (User) mav.getModel().get("user");
 		assertThat(user.getName(), is("주현태"));
 	}
+
+	@Test
+	public void test2() throws ServletException, IOException {
+		ModelAndView mav = setLocations("classpath:config/*-context.xml", "classpath:web-context.xml")
+				.initRequest("/sample", RequestMethod.GET).addParameter("id", "1").runService().getModelAndView();
+		
+		User user = (User) mav.getModel().get("user");
+		assertThat(user.getName(), is("주현태"));
+	}
+
 }
--- src/test/java/com/copocalypse/www/web/testutil/AbstractDispatcherServletTest.java
+++ src/test/java/com/copocalypse/www/web/testutil/AbstractDispatcherServletTest.java
@@ -1,18 +1,27 @@
 package com.copocalypse.www.web.testutil;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import org.junit.After;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.mock.web.MockServletConfig;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
-public class AbstractDispatcherServletTest implements AfterRunService{
+public class AbstractDispatcherServletTest implements AfterRunService {
 	protected MockHttpServletRequest request;
 	protected MockHttpServletResponse response;
-	protected MockServletConfig config=new MockServletConfig("spring");
+	protected MockServletConfig config = new MockServletConfig("spring");
 	protected MockHttpSession session;
-	
 
 	private ConfigurableDispatcherServlet dispatcherServlet;
 	private Class<?>[] classes;
@@ -20,40 +29,176 @@ public class AbstractDispatcherServletTest implements AfterRunService{
 	private String[] relativeLocations;
 	private String servletPath;
 
+	public AbstractDispatcherServletTest initRequest(String requestUri, String method) {
+		this.request=new MockHttpServletRequest(method,requestUri);
+		this.response=new MockHttpServletResponse();
+		if(this.servletPath !=null) this.setServletPath(this.servletPath);
+		return this;
+	}
+	public AbstractDispatcherServletTest initRequest(String requestUri, RequestMethod method) {
+		return this.initRequest(requestUri, method.toString());
+	}
+	
+	public AbstractDispatcherServletTest initRequest(String requestUri) {
+		initRequest(requestUri,RequestMethod.GET);
+		return this;
+	}
+	
+	public AbstractDispatcherServletTest addParameter(String name, String value) {
+		if(this.request==null)
+			throw new IllegalStateException("request가 초기화되지 않았습니다.");
+		this.request.addParameter(name, value);
+		return this;
+	}
+	
+	public AbstractDispatcherServletTest buildDispatcherServlet() throws ServletException{
+		if(this.classes ==null && this.locations ==null && this.relativeLocations==null) {
+			throw new IllegalStateException("classes와 locations 중 하나는 설정해야 합니다.");
+			
+		}
+		
+		this.dispatcherServlet=new ConfigurableDispatcherServlet();
+		this.dispatcherServlet.setClasses(this.classes);
+		this.dispatcherServlet.setLocations(this.locations);
+		if(this.relativeLocations!=null)
+			this.dispatcherServlet.setRelativeLocations(getClass(), this.relativeLocations);
+		
+		this.dispatcherServlet.init(this.config);
+		
+		return this;
+	}
+	
+	public AfterRunService runService() throws ServletException, IOException{
+		if(this.dispatcherServlet ==null) buildDispatcherServlet();
+		if(this.request==null)
+			throw new IllegalStateException("request가 준비되지 않았습니다.");
+		this.dispatcherServlet.service(this.request, this.response);
+		return this;
+	}
+	
+	public MockHttpServletRequest getRequest() {
+		return request;
+	}
+
+	public void setRequest(MockHttpServletRequest request) {
+		this.request = request;
+	}
+
+	public MockHttpServletResponse getResponse() {
+		return response;
+	}
+
+	public void setResponse(MockHttpServletResponse response) {
+		this.response = response;
+	}
+
+	public MockServletConfig getConfig() {
+		return config;
+	}
+
+	public void setConfig(MockServletConfig config) {
+		this.config = config;
+	}
+
+	public MockHttpSession getSession() {
+		return session;
+	}
+
+	public void setSession(MockHttpSession session) {
+		this.session = session;
+	}
+
+	public ConfigurableDispatcherServlet getDispatcherServlet() {
+		return dispatcherServlet;
+	}
+
+	public void setDispatcherServlet(ConfigurableDispatcherServlet dispatcherServlet) {
+		this.dispatcherServlet = dispatcherServlet;
+	}
+
+	public Class<?>[] getClasses() {
+		return classes;
+	}
+
+	public AbstractDispatcherServletTest setClasses(Class<?>[] classes) {
+		this.classes = classes;
+		return this;
+	}
+
+	public String[] getLocations() {
+		return locations;
+	}
+
+	public AbstractDispatcherServletTest setLocations(String... locations) {
+		this.locations = locations;
+		return this;
+	}
+
+	public String[] getRelativeLocations() {
+		return relativeLocations;
+	}
+
+	public AbstractDispatcherServletTest setRelativeLocations(String... relativeLocations) {
+		this.relativeLocations = relativeLocations;
+		return this;
+	}
+
+	public String getServletPath() {
+		return servletPath;
+	}
+
+	public AbstractDispatcherServletTest setServletPath(String servletPath) {
+		if(this.request==null){
+			this.servletPath=servletPath;	
+		}
+		else {
+			this.request.setServletPath(servletPath);
+		}
+		return this;
+	}
+
 	@Override
 	public String getContentString() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public WebApplicationContext getContext() {
-		// TODO Auto-generated method stub
-		return null;
+		if(this.dispatcherServlet==null)
+			throw new IllegalStateException("DispatcherServlet이 준비되지 않았습니다.");
+		
+		return this.dispatcherServlet.getWebApplicationContext();
 	}
 
 	@Override
 	public <T> T getBean(Class<T> beanType) {
-		// TODO Auto-generated method stub
-		return null;
+		if(this.dispatcherServlet==null)
+			throw new IllegalStateException("DispatcherServlet이 준비되지 않았습니다.");
+		return this.getContext().getBean(beanType);
 	}
 
 	@Override
 	public ModelAndView getModelAndView() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.dispatcherServlet.getModelAndView();
 	}
 
 	@Override
 	public AfterRunService assertViewName(String viewName) {
-		// TODO Auto-generated method stub
-		return null;
+		assertThat(this.getModelAndView().getViewName(), is(viewName));
+		return this;
 	}
 
 	@Override
 	public AfterRunService assertModel(String name, Object value) {
-		// TODO Auto-generated method stub
-		return null;
+		assertThat(this.getModelAndView().getModel().get(name), is(value));
+		return this;
+	}
+	
+	@After
+	public void closeServletContext() {
+		if(this.dispatcherServlet!=null) {
+			((ConfigurableApplicationContext)dispatcherServlet.getWebApplicationContext()).close();
+		}
 	}
 
 }
